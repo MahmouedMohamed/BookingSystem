d3f105fc3ddd3181e304a061eba713c0720429b1
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-09-02 20:28:54 +0300

    init project

---
+++ database/migrations/2025_09_02_160302_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('provider_id')->references('id')->on('users');
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->tinyInteger('category')->nullable();
+            $table->integer('duration')->comment('IN Minutes');
+            $table->decimal('price');
+            $table->boolean('is_published')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
---
+++ database/migrations/2025_09_02_163621_create_provider_availability_overrides_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('provider_availability_overrides', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('provider_id')->references('id')->on('users');
+            $table->tinyInteger('weekday');
+            $table->time('start')->nullable();
+            $table->time('end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provider_availability_overrides');
+    }
+};
