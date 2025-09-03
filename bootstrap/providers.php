f202fd31eda23ffa61fdac523c3c8a9283cc0a97
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-09-03 06:15:05 +0300

    add missing telescope provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    App\Providers\AuthServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
