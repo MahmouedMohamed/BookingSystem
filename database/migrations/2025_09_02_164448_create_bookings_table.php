14e7c011f0245fd83637b27fcebebce69f7f6fcf
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-09-04 23:33:44 +0300

    add provider_id to booking

--- database/migrations/2025_09_02_164448_create_bookings_table.php
+++ database/migrations/2025_09_02_164448_create_bookings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->references('id')->on('users');
+            $table->foreignId('provider_id')->references('id')->on('users');
             $table->foreignId('service_id')->references('id')->on('services');
             $table->dateTime('start_date')->comment('IN UTC');
             $table->dateTime('end_date')->comment('IN UTC');
